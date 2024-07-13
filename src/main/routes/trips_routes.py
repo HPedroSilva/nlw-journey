14a039f05eb4fadac17dc24d17dcec6552cf9af0
Pedro Henrique <pedrohs21@outlook.com>
2024-07-12 21:29:11 -0300

    implements  controller with route

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -2,6 +2,7 @@ from flask import jsonify, Blueprint, request
 
 # Controllers
 from src.controllers.trip_creator import TripCreator
+from src.controllers.trip_finder import TripFinder
 
 # Repositories
 from src.models.repositories.trips_repository import TripsRepository
@@ -21,5 +22,17 @@ def create_trip():
     controller = TripCreator(trips_repository, emails_repository)
 
     response = controller.create(request.json)
+
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trips_routes_bp.route("/trips/<tripId>", methods=["GET"])
+def find_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    controller = TripFinder(trips_repository)
+
+    response = controller.find_trip_details(tripId)
 
     return jsonify(response["body"]), response["status_code"]
